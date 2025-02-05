7e017fd47d441a6c8b726d47fcb57cd97867104a
IcedTeaBoy3 <cuakicuc110193@gmail.com>
2025-02-05 19:33:31 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 // Middleware to enable CORS
 app.use(cors());
 // Middleware to parse JSON
@@ -12,4 +13,18 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+// Middleware to handle 404 Error
+app.use((req, res, next) => {
+  // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+  // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+  next(new ApiError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+  // Middleware xử lý lỗi tập trung
+  // Xử lý lỗi ở đây
+  res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 module.exports = app;
