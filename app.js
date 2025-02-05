773f3297bf2e7b334aff4243d9d0508ddf8f1ef7
IcedTeaBoy3 <cuakicuc110193@gmail.com>
2025-02-05 19:06:16 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 // Middleware to enable CORS
 app.use(cors());
 // Middleware to parse JSON
@@ -10,4 +11,5 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
 
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
